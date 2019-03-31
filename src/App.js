7a444554d10bd148dde6feba8a4d35851f757cc9
zhan2016 <ronzhang2015@gmail.com>
2019-03-31 11:07:32 +0800

    Base Framework build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -11,14 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+					<Button type="button" class="close" data-dismiss="alert" aria-label="Close">第一个按钮</Button>
         </header>
       </div>
     );
