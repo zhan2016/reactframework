d74edefe40a45d6dad2318aa783cf1120c1ae190
zhan2016 <ronzhang2015@gmail.com>
2019-03-31 12:29:13 +0800

    framework

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Button} from 'react-bootstrap';
+import {observable} from 'mobx'
+import {observer} from 'mobx-react'
 
+@observer
 class App extends Component {
   render() {
     return (
